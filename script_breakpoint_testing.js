50bf0c43d36a30e00e93ba2a0ace37058e7e3d8f
aofserver <mr.sarawutnacwijit@gmail.com>
2026-01-06 10:57:48 +0700

    update

--- script_breakpoint_testing.js
+++ script_breakpoint_testing.js
@@ -2,11 +2,33 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export const options = {
-  executor: 'ramping-arrival-rate', // การเพิ่มจำนวนของผู้ใช้เข้ามาในระบบอย่างค่อยเป็นค่อยไปในช่วงเวลาที่กำหนด 
-  stages: [
-    { duration: '2h', target: 20000 }, // ค่อยๆเพิ่มโหลดจนถึงจุดสูงสุด
-  ],
+  scenarios: {
+    breakpoint: {
+      executor: 'ramping-arrival-rate', // การเพิ่มจำนวนของผู้ใช้เข้ามาในระบบอย่างค่อยเป็นค่อยไปในช่วงเวลาที่กำหนด 
+      startRate: 50,
+      timeUnit: '1s',
+      stages: [
+        { target: 10000, duration: '1m' }, // ค่อยๆเพิ่มโหลดจนถึงจุดสูงสุด
+        { target: 20000, duration: '1m' },
+        { target: 30000, duration: '1m' },
+      ],
+      preAllocatedVUs: 200,
+      maxVUs: 2000,
+    },
+  },
+
+  thresholds: {
+    //ถ้า error เกิน 0% หยุดทันที
+    http_req_failed: [
+      {
+        threshold: 'rate==0',
+        abortOnFail: true,
+        delayAbortEval: '5s',
+      },
+    ],
+  },
 };
+
 
 export default function () {
   const res = http.get(
